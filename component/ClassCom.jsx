4e0229a135bbec653a4a593d167e9c46da58b623
komal-godhaniya <162454232+komal-godhaniya@users.noreply.github.com>
2024-08-30 16:50:51 +0530

    Add files via upload

---
+++ component/ClassCom.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+export default class ClassCom extends Component {
+  
+    constructor(props){
+        super(props)
+        this.state = {count : 0};
+    }
+    increment = () => {
+        this.setState({count : this.state.count + 1})
+    }
+  
+    render() {
+    return (
+        <div>
+            <h1>ClassCom</h1>
+            <h1>{this.state.count}</h1>
+            <button onClick = {this.increment}>Click</button>
+        </div>
+    )
+  }
+}
+
