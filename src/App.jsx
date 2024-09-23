40a01994d31c452e761c1154629943b444662252
komal-godhaniya <162454232+komal-godhaniya@users.noreply.github.com>
2024-09-23 16:00:31 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,9 @@
 import React from 'react'
-import Nav from './component/Nav'
-import Section from './component/Section'
-import Footer from './component/Footer'
+import Web from './Web/Web'
 
 export default function App() {
   return (
-
-    <>
-      <Nav/>
-      <Section/>
-      <Footer/>
-    </>
-    
-
-
+    <Web/>
   )
 }
+
