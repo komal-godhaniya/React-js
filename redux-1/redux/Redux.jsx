c7d95685f07e56f87cfb9c7cc6b2db77f14b047c
komal-godhaniya <162454232+komal-godhaniya@users.noreply.github.com>
2024-10-10 17:24:07 +0530

    Add files via upload

---
+++ redux-1/redux/Redux.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment } from '../features/Slice'
+
+export default function Redux() {
+    const count = useSelector((state)=>{
+        return state.counterKey
+    })
+
+    const dispatch = useDispatch()
+  return (
+    <div>
+        <h1>Redux</h1>
+        <h1>{count.count}</h1>  
+         <button onClick={()=> dispatch(increment())}>+</button>
+         <button onClick={()=> dispatch(decrement())}>-</button>
+    </div>
+  )
+}
